405e88bcac3786d5b9f2115b6a8c9d1310d8598c
Artem Lobanov <artemlobanov95@gmail.com>
2023-04-19 08:34:36 +0300

    mobil fix

--- js/js.js
+++ js/js.js
@@ -10,7 +10,9 @@ THREE.ImageUtils.crossOrigin = '';
 const texture = THREE.ImageUtils.loadTexture(textureURL);
 
 let speed = 0;
+let time = 0;
 let uniforms = {
+    time: {type:'f', value: time},
     speed:{type:'f', value: speed},
 	mouseCoord: {type:'v3', value: new THREE.Vector3()},
 	tex: { type:'t', value:texture },
@@ -25,10 +27,14 @@ scene.add(sprite);
 camera.position.z = 2;
 
 uniforms.mouseCoord.value.z = 50;
+
 function render() {
+    time += 0.1;
+    uniforms.time.value = time;
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	requestAnimationFrame( render );
 	renderer.render( scene, camera );
+    console.log('time:', time);
 }
 render();
 
@@ -40,31 +46,31 @@ let lastX = 0;
 let lastY = 0;
 const smoothing = 0.1;
 document.addEventListener('mousemove', function(event) {
-    uniforms.speed.value = speed;
-    console.log('Speed:', speed);
     const x = event.clientX;
     const y = event.clientY;
-
+    
     if (lastX !== 0 && lastY !== 0) {
         const newSpeed = Math.sqrt((x - lastX) ** 2 + (y - lastY) ** 2);
-        speed = lerp(speed, newSpeed, smoothing);
+        speed = newSpeed;
     }
 
     lastX = x;
     lastY = y;
+    uniforms.speed.value = speed;
 });
 
 let targetX = 0;
 let targetY = 0;
 let currentX = 0;
 let currentY = 0;
+const speedFactor = 0.5;
 
 function update() {
     requestAnimationFrame(update);
     const diffX = targetX - currentX;
     const diffY = targetY - currentY;
     const distance = Math.sqrt(diffX * diffX + diffY * diffY);
-    const speed = distance / 10;
+    const speed = distance * speedFactor;
     if (distance < 1.0) {
         currentX = targetX;
         currentY = targetY;
@@ -72,51 +78,54 @@ function update() {
         currentX += speed * diffX / distance;
         currentY += speed * diffY / distance;
     }
-    console.log(currentX);
 }
 update()
 
 document.addEventListener('mousemove', (event) => {
     targetX = event.clientX;
     targetY = event.clientY;
-    uniforms.mouseCoord.value.x = currentX;
-    uniforms.mouseCoord.value.y = -currentY + window.innerHeight;
 });
 
 
-const cursorDiv = document.createElement('div');
-cursorDiv.style.position = 'fixed';
-cursorDiv.style.top = '0';
-cursorDiv.style.left = '0';
-cursorDiv.style.width = '20px';
-cursorDiv.style.height = '20px';
-cursorDiv.style.backgroundColor = 'black';
-cursorDiv.style.opacity = '0.5';
-cursorDiv.style.borderRadius = '50%';
-document.body.appendChild(cursorDiv);
-
-let posX = 0;
-let posY = 0;
-let mouseX;
-let mouseY;
 function animate() {
     requestAnimationFrame(animate);
-    const mouseX = currentX;
-    const mouseY = currentY;
-    posX += (mouseX - posX) * 0.5;
-    posY += (mouseY - posY) * 0.5;
-
-    cursorDiv.style.left = posX - 10 + 'px';
-    cursorDiv.style.top = posY - 10 + 'px';
     uniforms.mouseCoord.value.x = currentX;
     uniforms.mouseCoord.value.y = -currentY + window.innerHeight;
 }
+animate();
 
-function mousemoveHandler(event) {
-  mouseX = event.clientX;
-  mouseY = event.clientY;
+
+let lastTouchX = 0;
+let lastTouchY = 0;
+const touchSmoothing = 0.1;
+
+// Функция-обработчик для событий touchstart и touchmove
+function touchHandler(event) {
+
+const firstTouch = event.touches[0] || event.changedTouches[0];
+targetX = firstTouch.clientX;
+targetY = firstTouch.clientY;
+
+// Вычисляем скорость для touch events
+if (lastTouchX !== 0 && lastTouchY !== 0) {
+const touchSpeed = Math.sqrt((targetX - lastTouchX) ** 2 + (targetY - lastTouchY) ** 2);
+speed = lerp(speed, touchSpeed, touchSmoothing);
 }
 
-document.addEventListener('mousemove', mousemoveHandler);
+lastTouchX = targetX;
+lastTouchY = targetY;
+uniforms.speed.value = speed;
 
-animate();
+uniforms.mouseCoord.value.x = currentX;
+uniforms.mouseCoord.value.y = -currentY + window.innerHeight;
+}
+
+// Назначаем обработчик событий
+document.addEventListener('touchstart', touchHandler, false);
+document.addEventListener('touchmove', touchHandler, false);
+document.addEventListener('touchend', touchHandler, false);
+
+// Объединяем обработчики событий mousemove и touchmove
+document.addEventListener('mousemove', (event) => {
+touchHandler(event);
+});
